1ad44e5b0c469efe4815985c32da2a04488a04df
Petr Andreev <drakula2805@gmail.com>
2024-07-14 14:37:22 +0300

    Обновил conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,7 @@
 import pytest
 from selenium import webdriver
 from selene import browser
+from selenium.webdriver.chrome import options
 
 
 @pytest.fixture(scope='function', autouse=True)
@@ -8,10 +9,7 @@ def browser_management():
     browser.config.base_url = 'https://demoqa.com'
     browser.config.window_width = 1920
     browser.config.window_height = 1080
-    driver_options = webdriver.ChromeOptions()
-    driver_options.page_load_strategy = 'eager'
-    driver_options.add_argument("--headless")
-    browser.config.driver_options = driver_options
+    options.page_load_strategy = 'eager'
 
     yield
     browser.quit()
