48a1e11914baa49848f7ca7a92659cd587758518
Petr Andreev <drakula2805@gmail.com>
2024-07-15 11:09:39 +0300

    Фикс conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -13,10 +13,9 @@ def browser_management():
         browser.config.base_url = 'https://demoqa.com'
         browser.config.window_width = 1920
         browser.config.window_height = 1080
-        driver_options = webdriver.ChromeOptions()
-        driver_options.page_load_strategy = 'eager'
-        driver_options.add_argument("--headless")
-        browser.config.driver_options = driver_options
+        options = Options()
+        options.page_load_strategy = 'eager'
+        #options.add_argument("--headless")
 
         options = Options()
         selenoid_capabilities = {
@@ -45,7 +44,7 @@ def browser_management():
         attach.add_html(browser)
 
     with allure.step('Добавляем видео к отчету'):
-        attach.add_video(browser)
+        attach.add_video(browser, video_url="https://selenoid.autotests.cloud/video/")
 
     with allure.step('Закрываем браузер'):
         browser.quit()
